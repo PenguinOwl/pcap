c51f8df0bda159e79dccf7d670123a8720ee1610
Henry <henryjboles@gmail.com>
2017-12-02 16:10:35 -0800

    Update get-comments.rb

--- get-comments.rb
+++ get-comments.rb
@@ -1,1 +1,18 @@
-
+require "yt"
+class YTinteract
+  def get()
+    print "API Key: " 
+    Yt.configure do |config|
+        config.api_key = gets.strip
+    end
+    $chars = ""
+    print "Channel ID: "
+    channel = Yt::Channel.new id: gets.strip
+    channel.videos.each do |vid|
+      vid.comment_threads.each do |thread|
+        $chars = $chars + " " + thread.text_display
+      end
+    end
+    return $chars
+  end
+end
