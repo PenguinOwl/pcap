2d63cc20290ac10f014a6cca503994bedebb81d6
Henry <henryjboles@gmail.com>
2017-12-04 20:31:34 -0800

    Update filter.rb

--- filter.rb
+++ filter.rb
@@ -12,12 +12,16 @@ def scan(text, ptrlist)
   file = File.new("log_" + Time.now.to_s + ".txt", "w+")
   words = text.split(" ")
   words.each_index { |i|
+    a = 0
     ptrlist.each { |pattern|
       if pattern.first == Pattern.new(words[i]).first and (pattern.list - Pattern.new(words[i]).list)) < (Pattern.new(words[i]).list.size / 2) then 
         words.delete_at(i)
+        a = 1
       end
     }
+    file.write(words[i]+" "+a.to_s+"\n")
   }
   puts words.join(" ")
+  file.close
 end
-  
+scan("hi i am a cool dudes",[Pattern.new("dude")])
