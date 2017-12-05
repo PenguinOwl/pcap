f1219a4a638affae1d8226ccf34ff82f776a1810
Henry <henryjboles@gmail.com>
2017-12-04 19:59:36 -0800

    Update filter.rb

--- filter.rb
+++ filter.rb
@@ -1,3 +1,10 @@
-class DText
-  attr_accesor
-def dialate(text)
+class Pattern
+  attr_accessor :list, :first, :full
+  def initalize(text)
+    @first = text[0]
+    @full = text
+    @list = text.split("")
+    @list.delete_at(0)
+  end
+end
+
