fe31791ff3b6c2e4294f128626985cca9a354e8e
BuildTools <henryjboles@gmail.com>
2017-12-04 19:53:15 -0800

    weeee

--- get-comments.rb
+++ get-comments.rb
@@ -1,18 +1,28 @@
 require "yt"
 class YTinteract
-  def get()
+  def get
     print "API Key: " 
     Yt.configure do |config|
         config.api_key = gets.strip
+        config.log_level = :debug
     end
     $chars = ""
     print "Channel ID: "
     channel = Yt::Channel.new id: gets.strip
     channel.videos.each do |vid|
+      puts "s"
+      begin
       vid.comment_threads.each do |thread|
+        if thread.can_reply? then
+        puts "a"
         $chars = $chars + " " + thread.text_display
+        end
+      end
+      rescue Yt::Errors::Forbidden
       end
     end
     return $chars
   end
 end
+
+puts YTinteract.new.get
