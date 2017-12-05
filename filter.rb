4a458a97d51ac80aeccb9ac76628e707c73af67a
Henry <henryjboles@gmail.com>
2017-12-04 20:20:50 -0800

    Update filter.rb

--- filter.rb
+++ filter.rb
@@ -1,10 +1,23 @@
 class Pattern
   attr_accessor :list, :first, :full
   def initalize(text)
+    text.downcase!
     @first = text[0]
     @full = text
     @list = text.split("")
     @list.delete_at(0)
   end
 end
-
+def scan(text, ptrlist)
+  file = File.new("log_" + Time.now.to_s + ".txt", "w+")
+  words = text.split(" ")
+  words.each_index { |i|
+    ptrlist.each { |pattern|
+      if pattern.first == Pattern.new(words[i]).first and (pattern.list - Pattern.new(words[i]).list)) < (Pattern.new(words[i]).list.size / 2) then 
+        words.delete_at(i)
+      end
+    }
+  }
+  puts words.join(" ")
+end
+  
