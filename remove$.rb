c138cc34c171874c0c398cbc42e5286ab0ac4427
Henry <henryjboles@gmail.com>
2017-12-07 18:52:50 -0800

    Update remove$.rb

--- remove$.rb
+++ remove$.rb
@@ -2,6 +2,11 @@
 x = gets.strip
 file = File.open(x,"r+")
 file2 = File.new(x.gsub(".txt","_new.txt"),"w+")
-file2.write(file.read.gsub(/(.+)\s-\s(.+)\s\$(.+)/) { if $3 != nil and $2 != nil and $1 != nil then $1.gsub("/\W/","") + " " + $2.gsub("/\W/","") + " " + $3.gsub("/\W/","") end })
+file2.write(file.read.gsub(/(.+)\s-\s(.+)\s\$(.+)/) { 
+  if $3 != nil and $2 != nil and $1 != nil
+    a1, a2, a3 = $1,$2,$3
+    a1.gsub("/\W/","") + " " + a2.gsub("/\W/","") + " " + a3.gsub("/\W/","") 
+  end 
+  })
 file.close
 file2.close
