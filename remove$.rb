76ee869cda91fb19570047cad77b5d7b0af21ec9
Henry <henryjboles@gmail.com>
2017-12-07 18:21:03 -0800

    Update remove$.rb

--- remove$.rb
+++ remove$.rb
@@ -1,7 +1,7 @@
 
 x = gets.strip
 file = File.open(x,"r+")
-file2 = File.new(x+"_new","w+")
+file2 = File.new(x.gsub(".txt","_new.txt"),"w+")
 file2.write(file.read.gsub(/(.+)\s-\s(.+)\s\$(.+)/, $1 + $2 + $3))
 file.close
 file2.close
