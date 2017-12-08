5032225a3c8eed5a03e7dbbc313d101445bb8495
Henry <henryjboles@gmail.com>
2017-12-07 19:24:44 -0800

    Update remove$.rb

--- remove$.rb
+++ remove$.rb
@@ -5,7 +5,7 @@ file2 = File.new(x.gsub(".txt","_new.txt"),"w+")
 file2.write(file.read.gsub(/(.+)\s-\s(.+)\s\$(.+)/) { 
   if $3 != nil and $2 != nil and $1 != nil
     a1, a2, a3 = $1,$2,$3
-    a1.gsub("/\W/","") + " " + a2.gsub("/\W/","") + " " + a3.gsub("/\W/","") 
+    a1.gsub("/\W/","") + "\t" + a2.gsub("/\W/","") + "\t" + a3.gsub("/\W/","") 
   end 
   })
 file.close
